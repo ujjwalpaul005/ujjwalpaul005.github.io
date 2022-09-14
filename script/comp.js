e2eae1167dff0c9e002a9f588057ad9731c9acc8
ujjwalpaul005 <ujjwalpaul005@gmail.com>
2022-09-14 22:19:04 +0530

    About added

--- script/comp.js
+++ script/comp.js
@@ -1,7 +1,31 @@
-let hamburger = document.querySelector("#menu");
-let link = document.querySelector("#link");
+let navbar = () =>{
+    return `
+        <div id="menu">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+        </div>
 
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active");
-    link.classList.toggle("active");
-})
+        <div id="icon">
+            <img src="./Image/Icon.png">
+        </div>
+
+        <div id="link">
+            <a href="#"><span>01. </span>About</a>
+            <a href="#"><span>02. </span>Projects</a>
+            <a href="#"><span>03. </span>Games</a>
+            <a href="#"><span>04. </span>Contact</a>
+        </div>
+        <button>Resume</button>
+    `
+}
+
+let footer = () =>{
+    return`
+    <p>Created from scratch with <img src="./Image/heart.png"></p>
+    <p>© Copyright 2022</p>
+    <p>All Right Reserved</p>
+    `
+}
+
+export {navbar, footer};
---
+++ script/index.js
@@ -0,0 +1,12 @@
+import {navbar,footer} from "./comp.js";
+
+document.getElementById("nav").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
+
+let hamburger = document.querySelector("#menu");
+let link = document.querySelector("#link");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    link.classList.toggle("active");
+})
