e4551e55f47464c454121a35f305e1b102b0de04
ujjwalpaul005 <ujjwalpaul005@gmail.com>
2022-09-19 20:03:09 +0530

    almost final

--- script/index.js
+++ script/index.js
@@ -15,5 +15,48 @@ let button = document.querySelectorAll("button");
 
 button.forEach((x) => {
     x.addEventListener("click", ()=>{
-    window.open(x.innerText)
+        let link = x.innerText;
+        let url;
+        if(link == "Resume"){
+            url = "https://drive.google.com/file/d/1Q_tEP6AO6qxIWG850LAC4Fei4GPjjq5j/view?usp=sharing"
+        }else if(link == "GitHub"){
+            url = "https://github.com/ujjwalpaul005"
+        }
+        else if(link == "Github"){
+
+            if(x.value == "blue"){
+                url = "https://github.com/ujjwalpaul005/materialistic-knife-835";
+            }else if(x.value == "boat"){
+                url = "https://github.com/ujjwalpaul005/boat-lifestyle_clone"
+            }else if(x.value == "weather"){
+                url = "https://github.com/ujjwalpaul005/"
+            }else if(x.value == "food"){
+                url = "https://github.com/ujjwalpaul005/foodApp"
+            }else if(x.value == "recipe"){
+                url = "https://github.com/ujjwalpaul005"
+            }else if(x.value == "payment"){
+                url = "https://github.com/ujjwalpaul005"
+            }
+
+        }else if(link == "Live"){
+
+            if(x.value == "blue"){
+                url = "https://tiny-lamington-704c8c.netlify.app/";
+            }else if(x.value == "boat"){
+                url = "https://glowing-mandazi-4b14bc.netlify.app/"
+            }else if(x.value == "weather"){
+                url = "https://sprightly-tanuki-4070cf.netlify.app/"
+            }else if(x.value == "food"){
+                url = "https://magenta-panda-922b47.netlify.app/"
+            }else if(x.value == "recipe"){
+                url = "https://vocal-sprinkles-1c6b68.netlify.app/day.html"
+            }else if(x.value == "payment"){
+                url = "https://magenta-naiad-a3185e.netlify.app/"
+            }
+
+        }
+        else if(link == "LinkedIn"){
+            url = "https://www.linkedin.com/in/ujjwal-paul-204027235/"
+        }
+        window.open(url);
     })})
